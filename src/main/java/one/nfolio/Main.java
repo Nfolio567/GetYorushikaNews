4dfd0ff64494f2d607355786ebf8f2b229f375f1
Nfolio <139557367+Nfolio567@users.noreply.github.com>
2025-06-28 12:08:05 +0900

    fix bug

--- src/main/java/one/nfolio/Main.java
+++ src/main/java/one/nfolio/Main.java
@@ -1,11 +1,10 @@
 package one.nfolio;
 
 // import JavaStandardLibrary
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.time.LocalDate;
 import java.util.*;
+import java.util.concurrent.atomic.AtomicReference;
 
 // import ListComparisonMethod
 import org.apache.commons.collections4.ListUtils;
@@ -30,81 +29,84 @@ import org.jsoup.select.Elements;
 // ガターの <icon src="AllIcons.Actions.Execute"/> アイコンをクリックします。
 public class Main {
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
-    public static void main(String[] args) {
-        List<List<String>> newsList = List.of(List.of(""));
-        List<String> newsUrls = List.of("");
-        List<String> diffNews = new ArrayList<>();
-        boolean status = true;
-        LocalDate date = null;
 
-        while (true) {
-            LocalDate yesterday = date;
+    public static void main(String[] args) throws InterruptedException {
+        final AtomicReference<List<YorushikaNewsItem>> newsList = new AtomicReference<>(List.of());
+        final AtomicReference<List<YorushikaNewsItem>> diffNews = new AtomicReference<>(List.of());
+        final boolean[] status = {true};
+        final AtomicReference<LocalDate> date = new AtomicReference<>(null);
 
-            date = LocalDate.now();
-            try {
-                if (yesterday == null || date.isAfter(yesterday)) {
-                    // prepare scraping
-                    Document document = Jsoup.connect("https://yorushika.com/news")
+        for (;;){
+            logger.info("do");
+            LocalDate yesterday = date.get();
+
+            date.set(LocalDate.now());
+            if (yesterday == null || date.get().isAfter(yesterday)) {
+
+                // prepare scraping
+                Document document = null;
+                try {
+                    document = Jsoup.connect("https://yorushika.com/news")
                             .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36")
                             .get();// GET
-                    Elements elements = document.select(".list--news > *");
+                } catch (IOException e) {
+                    logger.warn(e.toString());
+                }
+                Elements elements = Objects.requireNonNull(document).select(".list--news > *");
 
-                    // Get some Yorushika News
-                    GetYorushikaNews getNews = new GetYorushikaNews(elements, logger);
-                    List<List<String>> oldNewsList = newsList;
-                    List<String> oldNewsUrls = newsUrls;
-                    newsList = getNews.newsList();
-                    newsUrls = getNews.newsUrls();
-                    for (int i = 0; i < newsList.size(); i++) {
-                        newsList.get(i).add(newsUrls.get(i));
+                // Get some Yorushika News
+                GetYorushikaNews getNews = new GetYorushikaNews(elements, logger);
+                List<YorushikaNewsItem> oldNewsList = newsList.get();
+                newsList.set(getNews.newsList());
+
+                // Old***が空文字リストかどうか（初回起動かどうか）
+                boolean hasEmptyOldNews = oldNewsList.isEmpty();
+                logger.info("oldNews is: {}", hasEmptyOldNews);
+
+                // Get diff
+                if ((oldNewsList.equals(newsList.get()) && !hasEmptyOldNews)) {
+                    List<YorushikaNewsItem> bufferDiffNewsList = new ArrayList<>(ListUtils.subtract(newsList.get(), oldNewsList));
+
+                    diffNews.set(bufferDiffNewsList);
+                    logger.info("buffer: {}\nnew: {}\nold: {}\nhasEmpty: {}, debug", bufferDiffNewsList, newsList.get(), oldNewsList, hasEmptyOldNews);
+                }
+
+                String channelAccessToken = System.getenv("SEND_YORUSHIKA_NEWS_CHANNEL_ACCESS_TOKEN");
+
+                SendMessage sendMessage = new SendMessage(channelAccessToken, logger);
+                if (status[0]) {
+                    StringBuilder message = new StringBuilder();
+                    for (YorushikaNewsItem i : newsList.get()) {
+                        message.append(String.format("%s : %s\n - %s\n| %s\n", i.date, i.category, i.title, i.url));
                     }
-
-                    // Old***が空文字リストかどうか（初回起動かどうか）
-                    boolean hasEmptyOldNews = oldNewsList.stream().anyMatch(i -> i.stream().anyMatch(String::isEmpty));
-                    boolean hasEmptyOldUrls = oldNewsUrls.stream().anyMatch(String::isEmpty);
-
-                    // Get diff
-                    if ((oldNewsList != newsList && !hasEmptyOldNews) || (oldNewsUrls != newsUrls && !hasEmptyOldUrls)) {
-                        List<String> bufferDiffNewsList = new ArrayList<>();
-                        ListUtils.subtract(newsList, oldNewsList).forEach(i -> bufferDiffNewsList.add(i.getFirst()));
-
-                        List<String> bufferDiffNewsUrls = new ArrayList<>(ListUtils.subtract(newsUrls, oldNewsUrls));
-                        diffNews = bufferDiffNewsList;
-                        diffNews.addAll(bufferDiffNewsUrls);
-                        logger.info("buffer: {}\nnew: {}\nold: {}\nhasEmpty: {}, debug", bufferDiffNewsList, newsList, oldNewsList, hasEmptyOldNews);
-                    }
-
-                    String channelAccessToken = "";
-                    try (InputStream file = Main.class.getResourceAsStream("/.env")) {
-                        BufferedReader reader = new BufferedReader(new InputStreamReader(Objects.requireNonNull(file)));
-                        Properties properties = new Properties();
-                        properties.load(reader);
-                        channelAccessToken = properties.getProperty("CHANNEL_ACCESS_TOKEN");
-                    } catch (Exception e) {
-                        logger.warn(e.toString());
-                    }
-
-                    SendMessage sendMessage = new SendMessage(channelAccessToken, logger);
-                    if (status) {
-                        StringBuilder message = new StringBuilder();
-                        for (List<String> i : newsList) {
-                            message.append(String.format("%s : %s\n - %s\n| %s\n", i.getFirst(), i.get(1), i.get(2), i.get(3)));
-                        }
-                        sendMessage.pushMessage(message.toString());
-                        status = false;
-                    } else {
-                        if (!diffNews.isEmpty()) {
-                            sendMessage.pushMessage(String.format("%s : %s\n - %s\n| %s\n", diffNews.getFirst(), diffNews.get(1), diffNews.get(2), diffNews.get(3)));
-                        }
+                    sendMessage.pushMessage(message.toString());
+                    status[0] = false;
+                } else {
+                    if (!diffNews.get().isEmpty()) {
+                        sendMessage.pushMessage(String.format("%s : %s\n - %s\n| %s\n", diffNews.get().getFirst(), diffNews.get().get(1), diffNews.get().get(2), diffNews.get().get(3)));
                     }
                 }
-            } catch (Exception e) {
-                Objects.requireNonNull(logger).warn(e.toString());
-                break;
             }
+            Thread.sleep(60000);
         }
     }
 }
+
+
+class YorushikaNewsItem {
+    String date;
+    String category;
+    String title;
+    String url;
+
+    public YorushikaNewsItem (String date, String category, String title, String url) {
+        this.date = date;
+        this.category = category;
+        this.title = title;
+        this.url = url;
+    }
+}
+
 
 class GetYorushikaNews {
     Elements elements;
@@ -115,39 +117,24 @@ class GetYorushikaNews {
         this.logger = logger;
     }
 
-    List<List<String>> newsList() {
-        List<List<String>> allNewsElements = new ArrayList<>();
+    List<YorushikaNewsItem> newsList() {
+        List<YorushikaNewsItem> allNewsElements = new ArrayList<>();
         try {
             for (Element element : elements) {
                 final String newsDate = element.childNode(1).childNode(1).childNode(0).toString().replaceAll("^\\[", "").replaceAll("\\]$", "");
                 final String newsCategory = element.childNode(1).childNode(1).childNode(1).childNodes().toString().replaceAll("^\\[", "").replaceAll("\\]$", "");
                 final String newsTitle = element.childNode(1).childNode(3).childNodes().toString().replaceAll("^\\[", "").replaceAll("\\]$", "");
+                final String newsUrl = element.childNode(1).absUrl("href");
 
-                List<String> newsHeader = new ArrayList<>();
-                newsHeader.add(newsDate);
-                newsHeader.add(newsCategory);
-                newsHeader.add(newsTitle);
-                allNewsElements.add(newsHeader);
+                allNewsElements.add(new YorushikaNewsItem(newsDate, newsCategory, newsTitle, newsUrl));
             }
         }catch (Exception e) {
             logger.warn(e.toString());
         }
         return allNewsElements;
     }
-
-    List<String> newsUrls() {
-        List<String> allNewsUrls = new ArrayList<>();
-        try {
-            for (Element element : elements) {
-                String url = element.childNode(1).absUrl("href");
-                allNewsUrls.add(url);
-            }
-        } catch (Exception e) {
-            logger.warn(e.toString());
-        }
-        return allNewsUrls;
-    }
 }
+
 
 class SendMessage {
     String channelAccessToken;
