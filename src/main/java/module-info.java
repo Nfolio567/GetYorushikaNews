505c2d77f10e07ff343443081ee002d4b16511a0
Nfolio <139557367+Nfolio567@users.noreply.github.com>
2025-06-29 16:51:25 +0900

    actiond

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module yorushika.scraping.main {
+    requires java.base;
+    requires org.apache.commons.collections4;
+    requires org.jsoup;
+    requires org.slf4j;
+
+    requires org.apache.logging.log4j;
+    requires org.apache.logging.log4j.core;
+    requires org.apache.logging.log4j.layout.template.json;
+}
